84f39d61651974cf37f82be44343cee49579ea51
wells <qishan.wu.1@lexisnexis.com>
2016-09-27 12:57:51 +0800

    refs:

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 
-phpinfo();
+$mem_conf = [
+	['localhost', 11211, 10],
+	['localhost', 11212, 90]
+];
+
+function create_memcached($conf) {
+	$mem = new Memcached();
+	$mem->addServers($conf);
+	return $mem;
+}
+
+$n = 100;
 
 
-?>
+$mem = create_memcached(mem_conf);
+
+for ($i = 1; $i <= $n; $i++) {
+	$mem->set("k".$i, $i);
+}
+
+
+
+
+
+
+
+
+
+
+
